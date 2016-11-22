d3af085f3d02be6a14c2c22d0d580d999ae6f7d9
Zaine-S9 <zaine.kingi@stationery.co.nz>
2016-11-23 06:52:00 +1300

    add validation function for email and password schema, trim input, error messaging and minlength values on the first, last name and password schemas.

--- server/models/users.js
+++ server/models/users.js
@@ -6,27 +6,52 @@ DESC:       Main model for interacting with the USERS database collection.
 //File Dependencies ------------------------------------------------------------
 var mongoose = require('mongoose');
 
+//Define the email validation function.
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+//Define the password validation function.
+var validatePassword = function(password){
+  var re = /^\(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  return re.test(password)
+};
+
 //Define the USERS model schema ------------------------------------------------
 var usersSchema = mongoose.Schema({
     //First Name
     first_Name: {
         type: String,
-        required: true
+        minlength: 3,
+        trim: true,
+        required: 'First Name is required!'
     },
     //Last Name
     last_Name: {
         type: String,
-        required: true
+        minlength: 3,
+        trim: true,
+        required: 'Surname is required!'
     },
     //Email
     email: {
         type: String,
-        required: true
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required!',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     //Password
     password: {
         type: String,
-        required: true
+        trim: true,
+        minlength: 8,
+        required: 'A password is required!'
+        //validate: [validatePassword, 'Please enter a secure password!'],
+        //match: [/^\(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, 'Please enter a secure password!']
     },
     //Record creation date.
     create_date: {
