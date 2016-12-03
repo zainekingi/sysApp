f7c878fb9387088da3fab1f774541ca7624a5559
zaine-kingi <zainekingi@users.noreply.github.com>
2016-12-03 17:42:04 +1300

    add addUser controller function.

--- client/controllers/users_controller.js
+++ client/controllers/users_controller.js
@@ -1,9 +1,16 @@
 var sysApp = angular.module('sysApp');
 sysApp.controller('UsersController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
   console.log('UsersController loaded...');
+
   $scope.getUsers = function() {
     $http.get('/api/users').success(function(response){
       $scope.users = response;
+    });
+  }
+
+  $scope.addUser = function() {
+    $http.post('/api/users/', $scope.users).success(function(response) {
+      window.location.href = '/api/users'
     });
   }
 }]);
