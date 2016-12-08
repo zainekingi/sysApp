9d032a72dd13cfa4e19e2f6060c022998437011a
ZaineS9 <zaine@github.com>
2016-12-09 05:53:37 +1300

    update addUser scope function location to \'users.html\'.

--- client/controllers/users_controller.js
+++ client/controllers/users_controller.js
@@ -9,9 +9,9 @@ sysApp.controller('UsersController', ['$scope', '$http', '$location', '$routePar
     });
   }
   // Scope function to fetch a record from the database.
-  $scope.getUser = function(id) {
+  $scope.getUser = function() {
     // Get the record id.
-    var id = $routeParams.id;
+    var id = $routeParams.id; console.log('From the controller ' + id);
     $http.get('/api/users/' + id).success(function(response) {
       // Assign the user object as the response.
       $scope.users = response;
@@ -19,15 +19,14 @@ sysApp.controller('UsersController', ['$scope', '$http', '$location', '$routePar
   }
   // Scope function to add a record to the database.
   $scope.addUser = function() {
-    var id = $routeParams.id;
-    $http.post('/api/users/', $scope.users).success(function(response) {
-      window.location.href = '/#/user/details/' + id;
+    $http.post('/api/users/', $scope.user).success(function(response) {
+      window.location.href = '/#/users';
     });
   }
   // Scope function to update a record in the database.
   $scope.updateUser = function() {
     // Get current record ID.
-    var id = $routeParams.id;
+    var id = $routeParams._id;
     $http.put('/api/users/' + id, $scope.users).success(function(response) {
       // Redirect the user back to the updated user profile.
       window.location.href = '/#/user/details/' + id;
@@ -37,7 +36,7 @@ sysApp.controller('UsersController', ['$scope', '$http', '$location', '$routePar
   $scope.deleteUser = function(id) {
     $http.delete('/api/users/' + id).success(function(response) {
       // Redirect to the homepage.
-      window.location.href = '/'
+      window.location.href = '/#/users'
     });
   }
 
