335a7b1ccad3dc4a1cf0cd10e1a5f8db4ed4402e
ZaineS9 <zaine@github.com>
2017-01-19 20:51:18 +1300

    add schools model methods POST, PUT, GET,GET BY ID, DELETE

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ var express     = require('express'),
     app         = express(),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
-    Users       = require('./models/users');
+    Users       = require('./models/users'),
+    Schools     = require('./models/schools');
 
 //Set static directory -------------------------------------------------------
 app.use(express.static(__dirname+'/../client'));
@@ -26,7 +27,7 @@ app.get('/', function(req, res, next) {
 //START: USER MODEL ==========================================================
 //Create user.
 app.post('/api/users', function(req, res) {
-    //Store post form data.
+    //Store form post data.
     var user = req.body;
     Users.addUser(user, function(err, user) {
         //Check for errors.
@@ -86,6 +87,75 @@ app.delete('/api/users/:_id', function(req, res) {
     });
 });
 //END: USER MODEL ============================================================
+
+//START: SCHOOL MODEL ========================================================
+//Create School.
+app.post('/api/schools', function(req, res){
+    //Store post form data.
+    var school = req.body;
+    //Call the Schools model addSchool method.
+    Schools.addSchool(school, function(err, school){
+        //Check for errors in the request data.
+        if(err) {
+            throw err;
+        }
+        //If no errors, send the stored form data as the response, formatting as JSON.
+        res.json(school);
+    });
+});
+//Get School.
+app.get('/api/schools', function(req,res) {
+    Schools.getSchools(function(err, schools) {
+        //Check if the collection exists and if it has any records.
+        if(err) {
+            throw err;
+        }
+        //No errors occurred, send response.
+        res.json(schools);
+    });
+});
+//Get School by ID.
+app.get('/api/schools/:_id', function(req, res) {
+    //Get the ID from the URL parameter.
+    var id = req.params._id;
+    Schools.getSchoolById(id, function(err, school) {
+        //Check if an ID is returned from the database.
+        if(err) {
+            throw err;
+        }
+        //No errors occurred, send response.
+        res.json(school);
+    });
+});
+//Update School.
+app.put('/api/school/:_id', function(req, res) {
+    //Get the ID from the URL parameter.
+    var id = req.params._id;
+    //Store the post form data.
+    var school = req.body;
+    Schools.updateSchool(id, school, {}, function(err, school) {
+        //Check for errors in the post form data.
+        if(err) {
+            throw err;
+        }
+        //No errors occurred, send response.
+        res.json(school);
+    });
+});
+//Delete School.
+app.delete('/api/schools/:_id', function(req, res) {
+    var id = req.params._id;
+    Schools.deleteSchool(id, function(err, school) {
+        //Check for any errors.
+        if(err) {
+            throw err;
+        }
+        //No errors occurred, send response.
+        res.json(school);
+    });
+});
+//END: SCHOOL MODEL ==========================================================
+
 //Application Server ---------------------------------------------------------
 mongoose.connect('mongodb://localhost/sysApp');
 var db = mongoose.connect;
