63a65bd7960194a6ff33e39a41a67a7a0d3ae14a
ZaineS9 <zaine@github.com>
2017-02-10 19:29:36 +1300

    fix bug with school details route.

---
+++ client/app.js
@@ -0,0 +1,40 @@
+var sysApp = angular.module('sysApp', ['ngRoute']);
+
+sysApp.config(function($routeProvider){
+    $routeProvider
+      .when('/', {
+      controller: 'SchoolsController',
+      templateUrl: 'views/home.html'
+    })
+    .when('/login', {
+      controller: 'UsersController',
+      templateUrl: 'views/login.html'
+    })
+    .when('/register', {
+      controller: 'UsersController',
+      templateUrl: 'views/register.html'
+    })
+    .when('/users', {
+        controller: 'UsersController',
+        templateUrl: 'views/users.html'
+    })
+    .when('/user/details/:id', {
+      controller: 'UsersController',
+      templateUrl: 'views/user_details.html'
+    })
+    .when('/user/edit/:id', {
+      controller: 'UsersController',
+      templateUrl: 'views/edit_details.html'
+    })
+    .when('/schools', {
+        controller: 'SchoolsController',
+        templateUrl: 'views/home.html'
+    })
+    .when('/schools/details/:id', {
+        controller: 'schoolsController',
+        templateUrl: 'views/school_details.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
